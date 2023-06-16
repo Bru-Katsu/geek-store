f30fa7335d7bb9850014cd1d286f17d2b13c01c4
Bruno Alves <bruno.alves5009@gmail.com>
2023-06-15 21:42:34 -0300

    Alterações do validador para comportar mensagens no input

--- site/geek-store/src/app/management/products/pages/product-editor/product-editor.component.ts
+++ site/geek-store/src/app/management/products/pages/product-editor/product-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subject, catchError, combineLatest, map, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, catchError, combineLatest, filter, map, startWith, switchMap, take, tap } from 'rxjs';
 import { ProductDetail } from 'src/app/shared/api-services/models/product-detail';
 import { ProductApiService } from 'src/app/shared/api-services/services/product-api.service';
 import { useValidations } from 'src/app/shared/operators/form-validation';
@@ -16,28 +16,42 @@ export class ProductEditorComponent implements OnInit {
   constructor(private route: ActivatedRoute, private productApi: ProductApiService, private fb: FormBuilder) { }
 
   loading$ = new Subject<boolean>();
+  saving$ = new Subject<boolean>();
   product$!: Observable<ProductDetail>;
   form$!: Observable<FormGroup>;
 
-  saveProduct$ = new Subject<ProductDetail>().pipe(switchMap(product => this.productApi.updateProduct(product)));
+  onEdited$ = new Subject<void>();
 
   ngOnInit(): void {
-    this.product$ = this.productApi.getProduct(this.route.snapshot.params['id']).pipe(
-      indicate(this.loading$)
+    this.product$ = this.onEdited$.pipe(
+      startWith(undefined),
+      indicate(this.saving$),
+      switchMap(() => this.route.paramMap),
+      switchMap(params => {
+        const productId = params.get('id') ?? '';
+        return this.productApi.getProduct(productId);
+      })
     );
 
-    this.form$ = this.product$.pipe(map(product => this.fb.group(product)),
-                                    useValidations({
-                                      name: [Validators.required, Validators.maxLength(150)],
-                                      price: [Validators.required],
-                                      category: [Validators.maxLength(50)],
-                                      description: [Validators.maxLength(500)]
-                                    }));
-
-
+    this.form$ = this.product$.pipe(
+      map(product => this.fb.group(product)),
+      useValidations({
+        name: [
+          [ Validators.required, "Informe o nome" ],
+          [ Validators.maxLength(150), "O nome deve conter no máximo 150 caracteres" ]
+        ],
+        price: [[ Validators.required, "Informe o preço do produto" ]],
+        category: [[ Validators.maxLength(50), "A categoria deve conter no máximo 50 caracteres" ]],
+        description: [[ Validators.maxLength(500), "A descrição deve conter no máximo 500 caracteres"]]
+      })
+    );
   }
 
-  save(form: ProductDetail){
-    return this.productApi.updateProduct(form);
+  save(product: ProductDetail){
+    this.productApi.updateProduct(product).pipe(
+      indicate(this.loading$),
+      tap(() => this.onEdited$.next()),
+      take(1)
+    ).subscribe();
   }
 }
--- site/geek-store/src/app/shared/operators/form-validation.ts
+++ site/geek-store/src/app/shared/operators/form-validation.ts
@@ -3,7 +3,12 @@ import { Observable, OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export interface ValidationMap {
-  [key: string]: ValidatorFn[];
+  [key: string]: ValidationProperty[];
+}
+
+export interface ValidationProperty {
+  0: ValidatorFn;
+  1: string;
 }
 
 export function useValidations<T extends { [K in keyof T]: AbstractControl<any, any>; }>(validations: ValidationMap): OperatorFunction<FormGroup<T>, FormGroup<T>> {
@@ -13,7 +18,9 @@ export function useValidations<T extends { [K in keyof T]: AbstractControl<any, 
         for (const key in validations) {
           const control = formGroup.get(key);
           if (control) {
-            control.setValidators(validations[key]);
+            const props = validations[key];
+
+            control.setValidators(props.map(p => p[0]));
             control.updateValueAndValidity();
           }
         }
@@ -22,3 +29,4 @@ export function useValidations<T extends { [K in keyof T]: AbstractControl<any, 
       })
     );
 }
+
